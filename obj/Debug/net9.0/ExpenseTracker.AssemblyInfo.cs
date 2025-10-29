f4ba99ecfda3e32bea3cb43a30a23029fd1e9837
sunil925business@gmail.com <sunil925business@gmail.com>
2025-10-29 15:56:59 +0530

    updated

--- obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c8fc636e3b8175dc8a09956411bf5a3d91af74")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
